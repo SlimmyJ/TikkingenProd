1431fabd42e323a3e301eb416aa355efa5efade6
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2025-10-23 12:53:31 +0200

    dded company icon in designer

--- designer.js
+++ designer.js
@@ -75,6 +75,7 @@ function setGridBackground() {
 }
 
 function iconFor(type) {
+  if (type === 'Bedrijf') return '<i class="fa-solid fa-building"></i>';
   if (type === 'Voertuig') return '<i class="fa-solid fa-car-side"></i>';
   if (type === 'Werf') return '<i class="fa-solid fa-person-digging"></i>';
   if (type === 'Huis') return '<i class="fa-solid fa-house"></i>';
